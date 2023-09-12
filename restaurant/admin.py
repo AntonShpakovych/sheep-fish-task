bba09b2df945bdd30b173d548bccb00ee06d78ff
Anton Shpakovych <shpakovych18@gmail.com>
2023-09-12 13:31:41 +0300

    add filtering for check on admin panel

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 from restaurant.models import Printer, Check, Point
 
 
+@admin.register(Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_filter = ["printer", "type", "status"]
+
+
 admin.site.register(Printer)
-admin.site.register(Check)
 admin.site.register(Point)
