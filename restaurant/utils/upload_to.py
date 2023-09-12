b90cc6b4a0e1c37d7994f1b749d36da4176a1d06
Anton Shpakovych <shpakovych18@gmail.com>
2023-09-12 14:50:03 +0300

    fixed pdf_path

--- restaurant/utils/upload_to.py
+++ restaurant/utils/upload_to.py
@@ -4,6 +4,6 @@ import os
 def pdf_path(instance, filename):
     _, extension = os.path.splitext(filename)
     return os.path.join(
-        "pdf//",
+        "pdf/",
         f"{instance.id}_{instance.type}"
     )
